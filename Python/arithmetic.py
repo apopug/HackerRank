4af474f6c919c3267d3ad01c815eba69ca06dec8
apopug <78620509+apopug@users.noreply.github.com>
2023-01-06 21:04:53 -0800

    Create arithmetic.py

---
+++ Python/arithmetic.py
@@ -0,0 +1,11 @@
+#Add code to print three lines where:
+#-The first line contains the sum of the two numbers.
+#-The second line contains the difference of the two numbers (first - second).
+#-The third line contains the product of the two number
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
